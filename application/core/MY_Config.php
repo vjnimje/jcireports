2791cf246cb198a4cada9d1e72c945f35cdcf630
Vijay Nimje <vjnimje@yahoo.com>
2021-12-30 09:43:11 +0530

    change

--- application/core/MY_Config.php
+++ application/core/MY_Config.php
@@ -21,7 +21,7 @@ class MY_Config extends CI_Config {
 
             else
             {
-                $base_url = 'http://localhost:71/';
+                $base_url = 'http://localhost:70/';
             }
 
             $this->set_item('base_url', $base_url);
